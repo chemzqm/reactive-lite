470c4d206fa73770d27d543e1900ad8fc37cfb42
chemzqm <chemzqm@gmail.com>
2015-10-28 11:37:29 +0800

    fix karma test

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'test/test.js',
+      'test/test_index.js'
     ],
 
     // list of files to exclude
@@ -25,7 +25,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      'test/test.js': ['webpack']
+      'test/test_index.js': ['webpack']
     },
 
     // test results reporter to use
